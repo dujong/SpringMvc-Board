a81e82b06e859f7aac8f20a9a961cae6d2892a18
jongdu <parksn5029@naver.com>
2023-06-03 19:11:45 +0900

    Add function: JWT + Spring Security
    Spring SecurityConfig 추가
    => FilterChain 기능 추가(페이지별 권한 인증, Filter 추가, csrf 보안 등)
    => PassWord 암호화 기능 추가
    
    JWT 기능 추가
    => JWT Service(토근 발급(재발급), 검사, 추출 등)
    => JWTAuthentication Filter 추가
    => jwt environment add
    => Token 정상 작동 Test 추가

---
+++ src/main/java/springmvc/board/global/config/SecurityConfig.java
@@ -0,0 +1,93 @@
+package springmvc.board.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import springmvc.board.domain.member.repository.MemberRepository;
+import springmvc.board.domain.member.service.LoginService;
+import springmvc.board.global.jwt.filter.JwtAuthenticationProcessingFilter;
+import springmvc.board.global.jwt.service.JwtService;
+import springmvc.board.global.login.filter.JsonUsernamePasswordAuthenticationFilter;
+import springmvc.board.global.login.handler.LoginFailureHandler;
+import springmvc.board.global.login.handler.LoginSuccessJWTProvideHandler;
+
+import javax.servlet.FilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig{
+
+    private final LoginService loginService;
+    private final ObjectMapper objectMapper;
+    private final JwtService jwtService;
+    private final MemberRepository memberRepository;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/login", "/signUp").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(jsonUsernamePasswordLoginFilter(), LogoutFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(loginService);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public LoginSuccessJWTProvideHandler loginSuccessJWTProvideHandler() {
+        return new LoginSuccessJWTProvideHandler(jwtService, memberRepository);
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler();
+    }
+
+    @Bean
+    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter(){
+        JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter = new JsonUsernamePasswordAuthenticationFilter(objectMapper);
+        jsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
+        jsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(loginSuccessJWTProvideHandler());
+        jsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(loginFailureHandler());
+        return jsonUsernamePasswordLoginFilter;
+    }
+
+    @Bean
+    public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter(){
+        JwtAuthenticationProcessingFilter jsonUsernamePasswordLoginFilter = new JwtAuthenticationProcessingFilter(jwtService, memberRepository);
+        return jsonUsernamePasswordLoginFilter;
+    }
+
+}
---
+++ src/main/java/springmvc/board/global/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -0,0 +1,95 @@
+package springmvc.board.global.jwt.filter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+import springmvc.board.domain.member.Member;
+import springmvc.board.domain.member.repository.MemberRepository;
+import springmvc.board.global.jwt.service.JwtService;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
+    private final String NO_CHECK_URL = "/login";
+    private final JwtService jwtService;
+    private final MemberRepository memberRepository;
+    private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        /*
+        NO CHECK URL이면 PASS
+        아래의 경우 /login 페이지에서는 체크하지 않음
+         */
+        if (request.getRequestURI().equals(NO_CHECK_URL)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        /*
+        RefreshToken이 없거나 유효하지 않다면 null 반환
+         */
+        String refreshToken = jwtService.extractRefreshToken(request)
+                .filter(jwtService::isTokenValid)
+                .orElse(null);
+
+        /*
+        refreshToken이 유효하다면 해당 refreshToken의 유저정보를 찾아오고, 존재한다면 AccessToken 발급
+         */
+        if(refreshToken != null){
+            checkRefreshTokenAndReIssueAccessToken(response, refreshToken);
+            return;
+        }
+
+        /*
+        refreshToken이 없다면 AccessToken을 검사하는 로직
+         */
+        checkAccessTokenAndAuthentication(request, response, filterChain);
+    }
+
+
+    private void saveAuthentication(Member member) {
+        UserDetails user = User.builder()
+                .username(member.getUsername())
+                .password(member.getPassword())
+                .roles(member.getRole().name())
+                .build();
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, authoritiesMapper.mapAuthorities(user.getAuthorities()));
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+    }
+
+    private void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken){
+        memberRepository.findByRefreshToken(refreshToken).ifPresent(
+                member -> jwtService.sendAccessToken(response, jwtService.createAccessToken(member.getUsername())
+                ));
+    }
+
+    private void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        jwtService.extractAccessToken(request).filter(jwtService::isTokenValid).ifPresent(
+                accessToken -> jwtService.extractUsername(accessToken).ifPresent(
+                        username -> memberRepository.findByUsername(username).ifPresent(
+                                this::saveAuthentication
+                        )
+                )
+        );
+        filterChain.doFilter(request, response);
+    }
+
+
+}
